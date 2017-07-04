c34fcc2980739c76a904cca580b13de3e614291b
Bence <oszb92@gmail.com>
2017-07-04 11:09:18 +0200

    Screen handling

--- MainScreen.py
+++
@@ -1,7 +0,0 @@
-from kivy.uix.boxlayout import BoxLayout
-
-
-class Main(BoxLayout):
-
-    def __init__(self):
-        super(Main, self).__init__()
--- main.py
+++ main.py
@@ -3,23 +3,38 @@ kivy.require('1.0.6')
 
 from kivy.app import App
 from kivy.config import Config
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-from MainScreen import *
+# Config app
+# to run on opengl 1.1 as well
+Config.set('graphics', 'multisamples', '0')
+# set window size
+Config.set('graphics', 'width', 480)
+Config.set('graphics', 'height', 320)
+# apply config settings
+Config.write()
 
+# Load UI declarations
+Builder.load_file('MainScreen.kv')
+Builder.load_file('StorageScreen.kv')
+
+# Declare all screens
+class MainScreen(Screen):
+    pass
+
+class StorageScreen(Screen):
+    pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MainScreen(name='main'))
+sm.add_widget(StorageScreen(name='storage'))
 
 class MainApp(App):
 
     def build(self):
-        return Main()
-
+        return sm
 
 if __name__ == '__main__':
-    # to run on opengl 1.1 as well
-    Config.set('graphics', 'multisamples', '0')
-    # set window size
-    Config.set('graphics', 'width', 480)
-    Config.set('graphics', 'height', 320)
-    # apply config settings
-    Config.write()
-    # run app
     MainApp().run()
